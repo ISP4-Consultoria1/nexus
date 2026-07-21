edc0c260d3406e884f00591e0c16b3c1280e52f7
lucasquaresm1 <lucasquaresm1@gmail.com>
2026-07-21 16:31:31 -0300

    chore(config): move database credentials to environment

--- functions/db.js
+++ functions/db.js
@@ -1,6 +1,10 @@
 import { neon, Pool } from '@neondatabase/serverless';
 
-const connectionString = 'postgresql://neondb_owner:npg_ds4tZlk6rEbC@ep-falling-union-acz48i0q-pooler.sa-east-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require';
+const connectionString = process.env.DATABASE_URL;
+
+if (!connectionString) {
+  throw new Error('A variável de ambiente DATABASE_URL não foi configurada.');
+}
 
 // Client SQL serverless (HTTP, recomendado para queries rápidas no Neon)
 export const sql = neon(connectionString);
